62c268479bf5b72f3b30a7cc63daefeb19a89d28
amig05c <amitghosh0595c@gmail.com>
2023-01-16 23:41:06 +0530

    check-xl-file

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const { connection } = require("./config/db")
 const app=express()
 app.use(express.json())
 
+app.post("/",(req,res)=>{
+  console.log(req.body)
+  res.send("done")
+})
+
 app.listen(8080,async()=>{
     try{
     await connection
